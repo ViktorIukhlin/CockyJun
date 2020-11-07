0c1d6986d6b44c69fe9133d79a451ca8359c20c7
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-11-07 22:30:49 +0300

    end button

--- src/components/QuestionsPage/QuestionsPage.jsx
+++ src/components/QuestionsPage/QuestionsPage.jsx
@@ -1,46 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { NavLink, Redirect } from 'react-router-dom'
-import Question from './Question/Question'
-import style from './QuestionsPage.module.scss'
-import ResultWindow from './ResultWindow/ResultWindow'
+import React, { useEffect, useState } from "react";
+import { Link, NavLink, Redirect } from "react-router-dom";
+import Question from "./Question/Question";
+import style from "./QuestionsPage.module.scss";
+import ResultWindow from "./ResultWindow/ResultWindow";
 
 const QuestionsPage = ({ listOfQuestions }) => {
   const [answers, setAnswers] = useState(listOfQuestions);
   const [counter, setCounter] = useState(0);
   const [resultWindow, setResultWindow] = useState(false);
-  const [answerFromPlayer, setAnswerFromPlayer] = useState('');
-  const [wrongAnswer, setWrongAnswer] = useState(0); 
+  const [answerFromPlayer, setAnswerFromPlayer] = useState("");
+  const [wrongAnswer, setWrongAnswer] = useState(0);
   // useEffect(() => {
   //   console.log(answers)
   //   console.log('RANDOM')
   // }, []);
   const sendAnswers = (e) => {
-    setAnswerFromPlayer(e.target.defaultValue)
-    setResultWindow(true)
-  }
+    setAnswerFromPlayer(e.target.defaultValue);
+    setResultWindow(true);
+  };
   const nextQuestionTrue = () => {
-    setResultWindow(false)
-    setCounter(counter + 1)
-  }
+    setResultWindow(false);
+    setCounter(counter + 1);
+  };
   const nextQuestionFalse = () => {
-    setWrongAnswer(wrongAnswer + 1)
-    setResultWindow(false)
-    setCounter(counter + 1)
-  }
+    setWrongAnswer(wrongAnswer + 1);
+    setResultWindow(false);
+    setCounter(counter + 1);
+  };
   return (
     <div className={style.container}>
       {counter === answers.length ? (
         <div className={style.containerCopy}>
           Правильных ответов {counter - wrongAnswer} из {counter}
-          <NavLink to="/startPage"> Заново </NavLink>
+          <Link to="/startPage" className={style.buttonReturn}>Заново</Link>
         </div>
-      ): resultWindow ? (
-        <ResultWindow answer={answerFromPlayer} rightAnswer={answers[counter].correctAnswer} nextQuestionTrue={nextQuestionTrue} nextQuestionFalse={nextQuestionFalse}/>
+      ) : resultWindow ? (
+        <ResultWindow
+          answer={answerFromPlayer}
+          rightAnswer={answers[counter].correctAnswer}
+          nextQuestionTrue={nextQuestionTrue}
+          nextQuestionFalse={nextQuestionFalse}
+        />
       ) : (
-        <Question sendAnswers={sendAnswers} answer={answers[counter]}/>
+        <Question sendAnswers={sendAnswers} answer={answers[counter]} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default QuestionsPage
+export default QuestionsPage;
